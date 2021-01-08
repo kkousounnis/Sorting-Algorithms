d250430642e275ca0097868183f04413fac16b6a
Konstantinos <kwstas654321@gmail.com>
2021-01-08 02:15:44 +0200

    Quishort
    Ascending, Descending

--- SortingAlgorithms/src/sortingalgorithms/SortingAlgorithms.java
+++ SortingAlgorithms/src/sortingalgorithms/SortingAlgorithms.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sortingalgorithms;
 
 import java.util.ArrayList;
@@ -11,32 +6,49 @@ import sortingalgorithms.models.RandomTshirt;
 import sortingalgorithms.models.Tshirt;
 import sortingalgorithms.sortingalgorithms.QuickSort;
 
-/**
- *
- * @author konstantinos
- */
 public class SortingAlgorithms {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
         int numberOfRandomTshirt = 7;
         List<Tshirt> ListrandomTshirts = new ArrayList<>(numberOfRandomTshirt);
-        
-        for (int i = 0; i < numberOfRandomTshirt; i++) {
-            ListrandomTshirts.add(new RandomTshirt());
-            System.out.println(ListrandomTshirts.get(i));
-        }
-        
+
+        //Random tshirt list Ascending by size
+        ListrandomTshirts = createRandomList(ListrandomTshirts, numberOfRandomTshirt);
         System.out.println("\nQuick Sorted Array By Size Ascending");
-        quickSort.quickSortTShirts(ListrandomTshirts, 0, numberOfRandomTshirt-1, 1, true);
-        
+        quickSort.quickSortTShirts(ListrandomTshirts, 0, numberOfRandomTshirt - 1, 0, true);
+        printSortedList(ListrandomTshirts, numberOfRandomTshirt);
+
+        //Random tshirt list Descending by size
+        ListrandomTshirts = createRandomList(ListrandomTshirts, numberOfRandomTshirt);
+        System.out.println("\nQuick Sorted Array By Size Descending");
+        quickSort.quickSortTShirts(ListrandomTshirts, 0, numberOfRandomTshirt - 1, 0, false);
+        printSortedList(ListrandomTshirts, numberOfRandomTshirt);
+
+        //Random tshirt list Ascending by color
+        ListrandomTshirts = createRandomList(ListrandomTshirts, numberOfRandomTshirt);
+        System.out.println("\nQuick Sorted Array By Size Ascending");
+        quickSort.quickSortTShirts(ListrandomTshirts, 0, numberOfRandomTshirt - 1, 1, true);
+        printSortedList(ListrandomTshirts, numberOfRandomTshirt);
+    }
+
+    public static List<Tshirt> createRandomList(List<Tshirt> ListrandomTshirts,
+            int numberOfRandomTshirt) {
+        System.out.println("\nRandom Tshirt List.");
         for (int i = 0; i < numberOfRandomTshirt; i++) {
             ListrandomTshirts.add(new RandomTshirt());
+
             System.out.println(ListrandomTshirts.get(i));
         }
+        return (ListrandomTshirts);
     }
-    
+
+    public static void printSortedList(List<Tshirt> ListrandomTshirts,
+            int numberOfRandomTshirt) {
+        for (int i = 0; i < numberOfRandomTshirt; i++) {
+
+            System.out.println(ListrandomTshirts.get(i));
+        }
+
+    }
 }
