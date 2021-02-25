2f9178b75feac96865de3ab68206b77a4fe26b03
Konstantinos <kwstas654321@gmail.com>
2021-02-25 22:56:47 +0200

    Create BubbleSort.java

---
+++ SortingAlgorithms/src/sortingalgorithms/sortingalgorithms/BubbleSort.java
@@ -0,0 +1,10 @@
+package sortingalgorithms.sortingalgorithms;
+
+import java.util.List;
+import sortingalgorithms.models.Tshirt;
+
+public interface BubbleSort {
+
+    public List<Tshirt> bubbleSortTShirts(List<Tshirt> arr, int sortByAttribute, boolean isAsc);
+
+}
