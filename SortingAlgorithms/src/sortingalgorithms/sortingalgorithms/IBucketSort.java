34e4d0548e8f57f8f3752218f27098db706be685
Konstantinos <kwstas654321@gmail.com>
2021-02-25 22:57:58 +0200

    Delete IBucketSort.java

--- SortingAlgorithms/src/sortingalgorithms/sortingalgorithms/IBucketSort.java
+++
@@ -1,9 +0,0 @@
-package sortingalgorithms.sortingalgorithms;
-
-import java.util.List;
-import sortingalgorithms.models.Tshirt;
-
-public interface IBucketSort {
-
-    public List<Tshirt> bucketSort(List<Tshirt> tshirts, int maxValue, int sortByProperty, boolean sortType);
-}
